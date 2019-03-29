1070f8b5892edb3daaced3ad0e67fa147f2f714f
Owen Dando <owen.dando@ed.ac.uk>
2019-03-29 11:48:33 +0000

    Tidy.

--- src/ena_upload_preprocess.sh
+++ src/ena_upload_preprocess.sh
@@ -1,50 +1,26 @@
 #!/usr/bin/env bash
 #
-# The script will pre-process the sample read files into
-# a format for uploading to ENA. The procedure is as follows:
+# The script will pre-process sample read files into a format for uploading to
+# ENA. The procedure is as follows:
 #
-# - generate md5sum before copy the reads to out folder
-# - copy the reads to out folder
-# - check the md5sum after copy
+# - generate md5sums before copying the read files to output folder
+# - copy the read files to the output folder
+# - check the md5sums after copying
 # - for each sample:
-#   - find all read1/read2
-#   - check read1/read2 has the same number of files
+#   - find all read1/read2 files
+#   - check read1/read2 have the same number of files
 #   - for read1/read2
-#     - unzip and concat into one file
-#     - check number of rows in merge file is the sum of pre-merged read files
-#     - compress merged file
-#     - move to output folder
-#     - check the compressed merged file has the same number of rows as the uncompress merged file
-#   - check merged-compressed read1/read2 has the same number of rows
-# - Finish
-#
-# Tested with /srv/data/ghardingham/dreadd_phasel samples,
-# took around 12 hours to finish with 42 paired-read samples, in which
-# each pair of reads contains 3 read files.
-#
-# Note from Owen:
-# Multi-file case: /srv/data/ghardingham/dreadd_phasel
-# Single-file case: /srv/data/tspiresjones/app_tau
-#
-# Inputs:
-# - directory containing the read files: e.g. /srv/data/ghardingham/dreadd_phasel
-# - set of sample names: e.g. Gq_Cnt_A, Gs_4h_CNO_A, Gs_Cnt_A
-# - patterns that identify _1 and _2 files
-#
-# Process:
-# - make a copy of original files and work on those, rather than original files
-# - for each sample
-# - -  for “_1” “_2” files (bearing in mind how the two types are named)
-# - - - calculate number of lines in the original files
-# - - - unzip and concatenate original files
-# - - - count lines in merged file and check equal
-# - - - gzip new file and call <sample_name>_1/2.fastq.gz
-# - - check that final _1 and _2 files have same number of lines
-# - - check that final _1 and _2 files are different
-#
-# Output
-# - a folder containing, for each sample, <sample_name>_1.fastq.gz, <sample_name>_2.fastq.gz
-# - text file containing md5sums for the *.fastq.gz
+#     - unzip and concatenate into one file
+#     - check the number of lines in the merged file is the sum of pre-merged
+#     read files
+#     - compress the merged file
+#     - move compressed, merged file to output folder
+#     - check the compressed, merged file has the same number of lines as the
+#     uncompressed, merged file
+#   - check merged, compressed read1/read2 files have the same number of rows
+# - create a file in the output folder containing md5sums for the merged,
+# compressed read files
+# - finish
 
 set -o nounset
 set -o errexit
@@ -61,48 +37,44 @@ trap cleanup EXIT
 
 function usage {
   cat <<EOT
-The script will pre-process the sample read files into
-a format for uploading to ENA. The procedure is as follows:
+The script will pre-process sample read files into a format for uploading to
+ENA. The procedure is as follows:
 
-- generate md5sum before copy the reads to out folder
-- copy the reads to out folder
-- check the md5sum after copy
+- generate md5sums before copying the read files to output folder
+- copy the read files to the output folder
+- check the md5sums after copying
 - for each sample:
-  - find all read1/read2
-  - check read1/read2 has the same number of files
+  - find all read1/read2 files
+  - check read1/read2 have the same number of files
   - for read1/read2
-    - unzip and concat into one file
-    - check number of rows in merge file is the sum of pre-merged read files
-    - compress merged file
-    - move to output folder
-    - check the compressed merged file has the same number of rows as the uncompress merged file
-  - check merged-compressed read1/read2 has the same number of rows
-- Finish
+    - unzip and concatenate into one file
+    - check the number of lines in the merged file is the sum of pre-merged
+    read files
+    - compress the merged file
+    - move compressed, merged file to output folder
+    - check the compressed, merged file has the same number of lines as the
+    uncompressed, merged file
+  - check merged, compressed read1/read2 files have the same number of rows
+- create a file in the output folder containing md5sums for the merged,
+compressed read files
+- finish
 
 Usage:
   $(basename $0)
         -i input folder which contains the sample folders
-        -o output folder which contains the read-to-upload reads
-        -s space separated sample_names to be included. default: all samples in input folder.
-        -1 pattern for read 1 files. default: "*_1.fastq.gz"
-        -2 pattern for read 1 files. default: ""*_2.fastq.gz""
-        -p number of cores. default: 1
-        -t test-mode, which will use only the first 10 line from read files. default: false
-        -h see help page
-
-Example:
-    bash ena_upload_preprocess.sh  -i /srv/data/ghardingham/dreadd_phasel -o ~/tmp/dreadd_phasel -s "Gs_Cnt_A Gs_Cnt_B" -1 "*_1.fastq.gz" -2 "*_2.fastq.gz" -p 10
-
+        -o output folder which will contain the read files for upload
+        -s space separated sample names to be included. (default: all samples
+        in input folder)
+        -1 pattern for read 1 files. (default: "*_1.fastq.gz")
+        -2 pattern for read 1 files. (default: "*_2.fastq.gz")
+        -p number of cores. (default: 1)
+        -t test-mode, which will use only the first 10 lines from read files.
+        (default: false)
+        -h display help
 EOT
 }
 
-
-#DATA_DIR=/srv/data/ghardingham/dreadd_phasel
-
-#bash ena_upload_preprocess.sh  -i /srv/data/ghardingham/dreadd_phasel -o ~/tmp/dreadd_phasel_test -s "Gs_Cnt_A" -1 "*_1.fastq.gz" -2 "*_2.fastq.gz" -p 10
-#OUT_DIR=~/tmp/${DATA_DIR##*/}
-
-## default value
+## default values
 
 SAMPLE_NAMES=''
 READ1_IDENTIFIER="*_1.fastq.gz"
@@ -123,18 +95,15 @@ while getopts 'i:o:s:1:2:p:t' opt; do
   esac
 done
 
-if [ -z ${SAMPLE_NAMES} ]; then
-    echo "-s is not set, using all sample found in ${DATA_DIR}."
+if [ -z "${SAMPLE_NAMES}" ]; then
+    echo "-s is not set, using all sampleis found in ${DATA_DIR}."
     SAMPLE_NAMES=`ls ${DATA_DIR}| grep -v .txt | paste -sd " " -`
     echo ${SAMPLE_NAMES}
 fi
 
-
 FINAL_DIR=${OUT_DIR}/final
-#SAMPLE_NAMES="Gs_Cnt_A Gs_Cnt_B"
 
-
-# check data folder exists and not empty
+# check data folder exists and is not empty
 if [  ! -d "${DATA_DIR}" ]; then
     >&2 echo "Error: data folder <${DATA_DIR}> does not exist." && exit 1
 fi
@@ -146,28 +115,27 @@ for sample in ${SAMPLE_NAMES}; do
     fi
 done
 
-# check out folder does not exist
+# check output folder does not exist
 if [  -d "${OUT_DIR}" ]; then
-    >&2 echo "Error: out folder <${OUT_DIR}> already exist." && exit 1
+    >&2 echo "Error: output folder <${OUT_DIR}> already exists." && exit 1
 fi
 mkdir -p ${OUT_DIR} ${FINAL_DIR}
 
-
-# generate md5sum before copy the reads to out folder
-echo "Generate md5sum before copy the reads to out folder..."
+# generate md5sums before copying the read files to output folder
+echo "Generate md5sums before copying the read files to output folder..."
 mkdir -p ${OUT_DIR}/md5sum
 for sample in ${SAMPLE_NAMES}; do
     echo "cd ${DATA_DIR} && find ${sample} -type f -exec md5sum {} + > ${OUT_DIR}/md5sum/${sample}.chk"
 done | xargs -t -n 1 -P ${NUM_CORES} -I % bash -c "%"
 
-# we copy the samples to out folder
-echo "Copying reads to out folder..."
+# copy the samples to output folder
+echo "Copying reads to output folder..."
 for sample in ${SAMPLE_NAMES}; do
     echo "cp -R -t ${OUT_DIR} ${DATA_DIR}/${sample}"
 done | xargs -t -n 1 -P ${NUM_CORES} -I % bash -c "%"
 
-# we check the md5sum after copy
-echo "Checking md5sum of reads in out folder..."
+# check md5sums after copying
+echo "Checking md5sums of read files in output folder..."
 for sample in ${SAMPLE_NAMES}; do
     echo "cd ${OUT_DIR} && md5sum -c ${OUT_DIR}/md5sum/${sample}.chk"
 done | xargs -t -n 1 -P ${NUM_CORES} -I % bash -c "%" > ${OUT_DIR}/md5sum/check1.txt
@@ -178,11 +146,9 @@ if grep -q 'FAILED' "${OUT_DIR}/md5sum/check1.txt"; then
     exit 1
 fi
 
-
-
 set -a
 function merge_reads(){
-    #If any invocation of the command exits with a status of 255,
+    # If any invocation of the command exits with a status of 255,
     # xargs will stop immediately without reading any further input.
     # An error message is issued on stderr when this happens
     tmp_dir=$1
@@ -197,7 +163,7 @@ function merge_reads(){
     out_file=${out_dir}/${sample}_${read_index}.fastq.gz
     rm -rf ${merged_file} ${merged_compress_file} ${out_file}
 
-    ## we unzip the reads and concat into one file
+    # unzip the reads and concat into one file
     if [ "$test_mode" = true ]; then
         for f in ${read_files}; do
             zcat -cdk ${f} | head -10 >>  ${merged_file}
@@ -208,8 +174,8 @@ function merge_reads(){
          done
     fi
 
-
-    ## check if merged reads file has the same number of rows as the sum of the pre-merged read files
+    # check if merged reads file has the same number of lines as the sum of
+    # the pre-merged read files
     lines_in_merged_file=`wc -l ${merged_file} | awk '{print $1}'`
 
     if [ "$test_mode" = true ]; then
@@ -222,31 +188,30 @@ function merge_reads(){
                               done | awk '{ SUM += $0} END { print SUM }'`
     fi
 
-
     if [ ! ${lines_in_merged_file} -eq ${lines_in_pre_merged_file} ];then
          >&2 echo "Error: number of rows differ after concatenating read files.  \
-         Excepting: ${lines_in_pre_merged_file}    " `basename ${merged_file}`": ${lines_in_merged_file} "
+         Expecting: ${lines_in_pre_merged_file}    " `basename ${merged_file}`": ${lines_in_merged_file} "
          exit 255
     fi
 
-    ## we compress the merged read files
+    # compress the merged read files
     gzip -fk ${merged_file}
 
-    ## move the compressed file to the output folder
+    # move the compressed file to the output folder
     mv ${merged_compress_file} ${out_file}
 
-    ## we check the compressed merged read files has the same number of rows as the umcompressed file
+    # check the compressed merged read files has the same number of rows as the
+    # uncompressed file
     lines_in_merged_compressed_out_file=`zcat ${out_file} | wc -l`
     if [ ! ${lines_in_merged_file} -eq ${lines_in_merged_compressed_out_file} ];then
          >&2 echo "Error: number of rows differ after compressing. \
-         Excepting : ${lines_in_merged_file}     " `basename ${merged_file}`": ${lines_in_merged_compressed_out_file} "
+         Expecting : ${lines_in_merged_file}     " `basename ${merged_file}`": ${lines_in_merged_compressed_out_file} "
          exit 255
     fi
-
 }
 
-function checkTwoPairEqualRow(){
-    ## check two fastq.gz has the same number of rows
+function check_two_pairs_equal_lines(){
+    ## check two fastq.gz files have the same number of rows
     read_1=$1
     read_2=$2
 
@@ -260,7 +225,6 @@ function checkTwoPairEqualRow(){
     fi
 }
 set +a
-
 
 echo "Merging samples..."
 for sample in ${SAMPLE_NAMES}; do
@@ -284,19 +248,19 @@ for sample in ${SAMPLE_NAMES}; do
     echo "merge_reads ${OUT_DIR} ${sample} 2 ${FINAL_DIR} ${TEST_MODE} " ${read_2_files}
 done |  xargs -t -n 1 -P ${NUM_CORES} -I % bash -c "%"
 
-
-## we check if the two final fastq.gz for each sample have the same number of rows
+# check if the two final fastq.gz files for each sample have the same number of
+# rows
 echo "Checking final reads has the same number of rows between pairs..."
 for sample in ${SAMPLE_NAMES}; do
-    echo "checkTwoPairEqualRow ${FINAL_DIR}/${sample}_1.fastq.gz ${FINAL_DIR}/${sample}_2.fastq.gz"
+    echo "check_two_pairs_equal_lines ${FINAL_DIR}/${sample}_1.fastq.gz ${FINAL_DIR}/${sample}_2.fastq.gz"
 done | xargs -t -n 1 -P ${NUM_CORES} -I % bash -c "%"
 
-## we generate md5sum for final fastq.gz
+# generate md5sums for the final fastq.gz files
 echo "Generate md5sum for final reads..."
 for sample in ${SAMPLE_NAMES}; do
     echo "cd ${FINAL_DIR} && md5sum ${sample}_1.fastq.gz"
     echo "cd ${FINAL_DIR} && md5sum ${sample}_2.fastq.gz"
 done | xargs -t -n 1 -P ${NUM_CORES} -I % bash -c "%" > ${FINAL_DIR}/md5sum.txt
 
-echo "Finished! The ready-to-upload reads can be found ${FINAL_DIR}"
+echo "Finished! The files for upload can be found in ${FINAL_DIR}"
 
