58bc5e8fe5d1d9460faee415ec37ecca824d0fff
hxin <hxin87@gmail.com>
2019-04-08 15:07:59 +0100

    bug fix.

--- src/ncbi_utils.sh
+++ src/ncbi_utils.sh
@@ -4,16 +4,19 @@
 function download_srr {
     local out_dir=$1
     local srr=$2
-    fastq-dump --split-3 --gzip --outdir ${out_dir} $srr
+    fastq-dump --split-3 --gzip --outdir ${out_dir}/$srr $srr
 }
 
 
+## @example: download_srrs /home/xinhe/tmp/liver 7 SRR998141 SRR998527 SRR998155 SRR998529 SRR998531 SRR998540 SRR998542
 function download_srrs {
-    local out_dir=$1; shift
-    local nc=$2; shift
-    local srrs=$@
+    local out_dir=$1
+    local nc=$2
+    shift 2
+
+    local srrs="$@"
 
     for srr in ${srrs}; do
         echo "download_srr ${out_dir} ${srr}"
-    done| xargs -t -n 1 -P ${nc} -I % bash -c "%"
+    done | xargs -t -n 1 -P ${nc} -I % bash -c "%"
 }
