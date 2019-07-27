7784f46ff132e4add40a14bb37f64922642ff778
hxin <hxin87@gmail.com>
2019-07-27 12:14:44 +0100

    download_srrs now runs fastqc after download.

--- src/ncbi_utils.sh
+++ src/ncbi_utils.sh
@@ -17,6 +17,17 @@ function download_srrs {
     local srrs="$@"
 
     for srr in ${srrs}; do
-        echo "download_srr ${out_dir} ${srr}"
+        echo "download_srr ${out_dir} ${srr} && fastqc --outdir=${out_dir} ${srr}.fastq.gz 2>${out_dir}/${srr}.log"
     done | xargs -t -n 1 -P ${nc} -I % bash -c "%"
 }
+
+function run_fastqc {
+    local dir=$1
+    local nc=${2:-1}
+    for r in `find ${dir} -name '*fastq.gz'`; do
+        echo fastqc --outdir=${r%/*} ${r}
+    done | xargs -t -n 1 -P ${nc} -I % bash -c "%"
+}
+
+
+
